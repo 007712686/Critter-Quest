05351f6a5b5ff916ca779c1edc91909c1f73849d
Andrew <42917293+CodeLinkCable@users.noreply.github.com>
2024-10-25 12:42:29 -0700

    Item Assign Added

---
+++ Assets/Inventory/ItemAssign.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemAssign : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Item itemItIs;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
