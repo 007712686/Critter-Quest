8bd2373bb6f79cd01a9b497882c9ebb528d2ecce
Andrew <42917293+CodeLinkCable@users.noreply.github.com>
2024-11-10 02:49:15 -0800

    Fix Commit Adds

---
+++ Assets/ShopStuff/ShopManager.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopManager : MonoBehaviour
+{
+    public int maxStackedItems = 16;
+    public InventorySlot[] inventorySlots;
+    public GameObject inventoryItemPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public bool AddItem(Item item)
+    {
+        //Find slot with stackable/same item
+        for (int i = 0; i < inventorySlots.Length; i++)
+        {
+            InventorySlot slot = inventorySlots[i];
+            InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
+            if (itemInSlot != null && itemInSlot.item == item && itemInSlot.count < maxStackedItems && itemInSlot.item.stackable == true)
+            {
+                itemInSlot.count++;
+                itemInSlot.RefreshCount();
+                return true;
+            }
+        }
+
+
+        //Find any empty slot
+        for (int i = 0; i < inventorySlots.Length; i++)
+        {
+            InventorySlot slot = inventorySlots[i];
+            InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
+            if (itemInSlot == null)
+            {
+                SpawnNewItem(item, slot);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void SpawnNewItem(Item item, InventorySlot slot)
+    {
+        GameObject newItemGo = Instantiate(inventoryItemPrefab, slot.transform);
+        InventoryItem inventoryItem = newItemGo.GetComponent<InventoryItem>();
+        inventoryItem.InitializeItem(item);
+    }
+
+
+    public void RemoveItem(InventoryItem itemToRemove)
+    {
+        if (itemToRemove.count > 1)
+        {
+            print("Goin");
+            itemToRemove.count--;
+            itemToRemove.RefreshCount();
+        }
+        else
+        {
+            Destroy(itemToRemove.gameObject);
+        }
+        Debug.Log("Item removed: " + itemToRemove.name);
+    }
+
+}
